8c247acc52ebc94d5dcd678b32ec06dd26ba6dde
Rose Landroche <50932371+roselandroche@users.noreply.github.com>
2021-10-18 12:34:38 -0700

    deletions, resetting up app.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,44 +1,24 @@
-import React from 'react';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+const express = require("express");
 
-import CreateUser from "./components/create-user.component";
-import Users from "./components/users.component";
+const app = express();
 
-function App() {
-  return (<Router>
-    <div className="App">
-      <header>
-        <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
-          <Link className="navbar-brand" to={"/"}>React Axios Tutorial</Link>
+app.get("/courses", (req, res) => {
+  res.status(200).json({ name: "api testing" });
+});
 
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item active">
-                <Link className="nav-link" to={"/create-user"}>Create User</Link>
-              </li>
-              <li className="nav-item">
-                <Link className="nav-link" to={"/users"}>Users List</Link>
-              </li>
-            </ul>
-          </div>
-        </nav>
-      </header>
+app.get("/course", (req, res) => {
+  let name = req.query.name;
 
-      <div className="container">
-        <div className="row">
-          <div className="col-md-12">
-            <Switch>
-              <Route exact path='/' component={Users} />
-              <Route path="/create-user" component={CreateUser} />
-              <Route path="/users" component={Users} />
-            </Switch>
-          </div>
-        </div>
-      </div>
-    </div>
-  </Router>
-  );
-}
+  res.json({ id: "1", name: name });
+});
 
-export default App;
+app.get("/course/:id", (req, res) => {
+  let id = req.params.id;
+  let name;
+  if (id === "1") {
+    name = "mocha";
+  }
+  res.json({ id: id, name: name });
+});
+
+module.exports = app;
